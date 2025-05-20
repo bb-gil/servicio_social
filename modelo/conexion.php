349ad5129a5005b4fbc66bb604d82654046ebe7f
bb-gil <alexisgil@colegioguaneta.edu.co>
2025-05-20 09:20:33 -0500

    se agrego servicio social remix

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,34 +1,34 @@
 <?php
-    // script para crear una conexión con la BD
+    // script para crear una conexion con la BD
 
-    // Parametros requeridos para la conexión con la BD
+    //Parametros requeridos para la conexion la BD
 
-    // Parametros BD local
-    define('USER', 'root'); //  Crea la constante USER con valor 'root'
-    define('PW', '');
-    define('HOST', 'localhost');
-    define('BD', 'servicio_social');
+    // Parámetros BD local
+    DEFINE('USER', 'root'); //Crea la constante USER con valor 'root'
+    DEFINE('PW', '');
+    DEFINE('HOST', 'localhost');
+    DEFINE('BD', 'Servicio_Social');
 
-    // Parametros BD remota (infinityfree)
-    /*define('USER', ''); 
-    define('PW', '');
-    define('HOST', '');
-    define('BD', '');*/
+    // Parámetros BD remota (infinityfree.com)
+    /*DEFINE('USER', 'if0_38542088'); //Crea la constante USER con valor 'if0_38542106'
+    DEFINE('PW', '');
+    DEFINE('HOST', 'sql100.infinityfree.com');
+    DEFINE('BD', 'if0_38542088_empresa');*/
 
-    // Conexión con la BD
-    $conexion = mysqli_connect(HOST, USER, PW, BD); 
+    // Conexion con la BD
+    $conexion = mysqli_connect(HOST, USER, PW, BD);
 
     // Establecer conjunto de caracteres para el hosting
-    mysqli_set_charset($conexion, 'utf8mb4'); 
+    mysqli_set_charset($conexion, "utf8mb4");
 
-    // Verificar la conexión con la BD
-
-if (!$conexion) 
+    // verificar la conexion con la BD
+    if(!$conexion)
     {
-        die("La conexión con la BD falló: " + mysqli_error($conexion));  
-    } 
-    /*else 
+        die("La conexión con la BD falló: " + mysqli_error($conexion));
+        exit();
+    }
+    /*else
     {
-        die("Conexión a la BD exitosa!"); 
+        die("Conexión exitosa a la BD!");
     }*/
 ?>
